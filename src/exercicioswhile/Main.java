f3c48d9d3b477d00b98bd17c23c9c323e1e04d3c
Lany Leite <Lanyleitee@gmail.com>
2024-01-29 12:53:31 -0300

    Teste de mesa while 05

--- src/exercicioswhile/Main.java
+++ src/exercicioswhile/Main.java
@@ -4,12 +4,12 @@ public class Main {
 
 	public static void main(String[] args) {
  
-		int x = 5;
+		int x = 2;
 		int y = 0;
-		while (x > 2) {
-		System.out.print(x);
-		y = y + x;
-		x = x - 1;
+		while (x <60) {
+		System.out.println(x);
+		x = x * 2;
+		y = y + 10;
 		}
 	}
 }
